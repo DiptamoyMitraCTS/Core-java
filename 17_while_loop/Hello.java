f2b3fb2c284e194505fb55e6e4dae2e97f310337
diptamoy mitra <mitradiptamoy70@gmail.com>
2023-08-06 03:20:51 +0530

    efsf

--- 17_while_loop/Hello.java
+++ 17_while_loop/Hello.java
@@ -1,13 +1,38 @@
 
-
 public class Hello {
   public static void main(String[] args) {
-    int i=1;
-   
-    while(i<=10)
-    {
+    int i = 1;
+
+    while (i <= 4) {
       System.out.println("Hi");
+      int j = 1;
+      while (j <= 3) {
+        System.out.println("hello");
+        j++;
+
+      }
       i++;
     }
+    System.out.println("Outside loop: " + i);
   }
 }
+
+/*
+ * Hi
+ * hello
+ * hello
+ * hello
+ * Hi
+ * hello
+ * hello
+ * hello
+ * Hi
+ * hello
+ * hello
+ * hello
+ * Hi
+ * hello
+ * hello
+ * hello
+ * Outside loop: 5
+ */
