a9a4baef0fb3af6a039c43af2e2380df9ffb05a6
diptamoy mitra <mitradiptamoy70@gmail.com>
2023-08-07 15:04:39 +0530

    addd

--- 19.For_Loop/Hello.java
+++ 19.For_Loop/Hello.java
@@ -1,11 +1,66 @@
 
-
 public class Hello {
   public static void main(String[] args) {
-    int n=5;
-    for (int j = 0; j < n; j++) {
-      System.out.println(j);
-      
+    int n = 5;
+    for (int j = 1; j <= n; j++) {
+      System.out.println("Day " + j);
+
+      for (int i = 1; i <= 9; i++) {
+        System.out.println(i);
+      }
+
     }
   }
 }
+/*
+ * Day 1
+ * 1
+ * 2
+ * 3
+ * 4
+ * 5
+ * 6
+ * 7
+ * 8
+ * 9
+ * Day 2
+ * 1
+ * 2
+ * 3
+ * 4
+ * 5
+ * 6
+ * 7
+ * 8
+ * 9
+ * Day 3
+ * 1
+ * 2
+ * 3
+ * 4
+ * 5
+ * 6
+ * 7
+ * 8
+ * 9
+ * Day 4
+ * 1
+ * 2
+ * 3
+ * 4
+ * 5
+ * 6
+ * 7
+ * 8
+ * 9
+ * Day 5
+ * 1
+ * 2
+ * 3
+ * 4
+ * 5
+ * 6
+ * 7
+ * 8
+ * 9
+ */
---
+++ 22.Class_objects_practical/Hero.java
@@ -0,0 +1,23 @@
+class Calc{
+  int a;
+  public int add(int x, int y){
+    System.out.println("In ADD");
+    return x+y;
+  }
+}
+
+
+class Hero{
+  public static void main(String[] args) {
+    int num1=4;
+    int num2=5;
+    // int res=num1+num2;
+    // System.out.println(res);
+    // add();  // error: cannot find symbol
+    Calc ob; //just create referenc;\
+    ob= new Calc(); //create object
+    System.out.println( ob.add(num1,num2));  //9
+ 
+
+  }
+}
