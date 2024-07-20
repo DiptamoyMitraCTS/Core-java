022e68425eea0f3247d7d362e55197d634b521aa
uplDiptamoy <diptamoy.mitra@underscoretec.com>
2024-07-20 19:49:22 +0530

    edit in literals

--- 7_Literals/Hello.java
+++ 7_Literals/Hello.java
@@ -1,14 +1,13 @@
 
-
 public class Hello {
   public static void main(String[] args) {
-     //literals
-    //  int num1=0x7E;   //126 
-    // int num1=10_00_00_000;   //100000000
-  // double num1=12e10;   //1.2E11
-  char num1='a';
-     System.out.println(num1); //a
-     num1++;
-      System.out.println(num1); //b
+    // literals
+    // int num1=0x7E; //126
+    // int num1=10_00_00_000; //100000000
+    // double num1=12e10; //1.2E11
+    char num1 = 'a';
+    System.out.println(num1); // a
+    num1++;
+    System.out.println(num1); // b
   }
 }
