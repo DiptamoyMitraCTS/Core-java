5ca19dfa3df259b5e14b4873f9c59347ef115cac
Diptamoy-Mitra <diptamoy.mitra.21@aot.edu.in>
2024-09-22 00:18:19 +0530

    upcasting

--- 59_upcasting_downcasting/Hero.java
+++ 59_upcasting_downcasting/Hero.java
@@ -17,23 +17,24 @@ public class Hero {
      public static void main(String[] args) {
          //type casting
         double d=8.9;
-        // int i=d; //not support
+        // int i=d; //not support //type mismatch
         int i=(int)d; //explicitly type casting
         System.out.println(i); //8
 
         //upcasting and downcasting
         A obj=new A();
-        obj.show1();
+        obj.show1(); //in A show1
+
         // obj.show2(); //not support because show2() is not in A class so it is not accessible by obj reference of A class
         
-        obj=new B(); //upcasting //B class object can be referred by A class reference variable obj      
+        obj=(A) new B(); //upcasting //B class object can be referred by A class reference variable obj 
+        obj.show1(); //in A show1
+
+        // obj.show2(); //not support because show2() is not in A class so it is not accessible by obj reference of A class //different method on A and B
+
+        B obj1=(B)obj; //downcasting //A class reference variable obj can be referred by B class object //obj is parent reference we downcast it to child reference
+        obj1.show1(); //in A show1 
+        obj1.show2(); //in B show2
         
-        B obj1=(B)obj; //downcasting //A class reference variable obj can be referred by B class object
-        obj1.show1(); //in A show1
-        
-
-
-
-
      }
 }
---
+++ 60_Wrapper_Class/Hero.java
@@ -0,0 +1,8 @@
+// WRAPPER CLASS
+// 1. Wrapper class is used to convert primitive data types into objects.
+
+public class Hero {
+    public static void main(String[] args) {
+        
+    }
+}
