3cee336cd15b4573fcdb92b3acfbb467422fee23
Diptamoy-Mitra <diptamoy.mitra.21@aot.edu.in>
2024-10-10 07:41:50 +0530

    upto what is interface

--- 71_what_is_interface/Hero.java
+++ 71_what_is_interface/Hero.java
@@ -1,12 +1,37 @@
 //what is interface
-
+//if only have abstract method
 interface A{
+    //all the method bydefault abstract and public
+    //interface just show us design
     public abstract void show();
     public abstract void config();
-} //
+
+    // int age; //error  face because they are bydefault final and static
+    int age=90;
+    String area="Mumbai";
+} 
+
+class B implements A{
+    //we need to must implements both method if not then the class becomes abstract
+    public void show(){
+        System.out.println("in show");
+    }
+    public void config(){
+        System.out.println("in config");
+    }
+}
 
 public class Hero {
     public static void main(String[] args) {
-        
+        A obj;
+        // obj=new A(); //can't instantiate
+
+        obj=new B();
+        obj.show(); //in show
+        obj.config(); // in config
+        System.out.println(A.area);//Mumbai
+      //  A.area="Delhi";//cannot assign a value to static final variable 
+
+
     }
 }
